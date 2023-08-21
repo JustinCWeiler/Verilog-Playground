a897f91c2a6d0b12940743d96d82d75c7e1dac16
Justin Weiler <jweiler@stanford.edu>
2023-08-20 22:21:19 -0700

    some changes

--- blink2/src/top.sv
+++ blink2/src/top.sv
@@ -3,14 +3,24 @@ module top (
 );
 
 	logic sys_clk;
-	SB_HFOSC u_SB_HFOSC (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(sys_clk));
-	defparam u_SB_HFOSC.CLKHF_DIV = "0b00";
+	SB_HFOSC #(
+		.CLKHF_DIV("0b00")
+	) u_SB_HFOSC (
+		.CLKHFPU(1'b1),
+		.CLKHFEN(1'b1),
+		.CLKHF(sys_clk)
+	);
 
 	logic led_r, led_g, led_b;
 
 	driver dut (.clk(sys_clk), .r(led_r), .g(led_g), .b(led_b));
 
-	SB_RGBA_DRV rgb (
+	SB_RGBA_DRV #(
+		.CURRENT_MODE("0b1"),
+		.RGB0_CURRENT("0b000001"),
+		.RGB1_CURRENT("0b000001"),
+		.RGB2_CURRENT("0b000001")
+	) rgb (
 		.RGBLEDEN (1'b1),
 		.RGB0PWM  (led_g),
 		.RGB1PWM  (led_b),
@@ -20,9 +30,5 @@ module top (
 		.RGB1     (led_blue),
 		.RGB2     (led_red)
 	);
-	defparam rgb.CURRENT_MODE = "0b1";
-	defparam rgb.RGB0_CURRENT = "0b000001";
-	defparam rgb.RGB1_CURRENT = "0b000001";
-	defparam rgb.RGB2_CURRENT = "0b000001";
 
 endmodule
