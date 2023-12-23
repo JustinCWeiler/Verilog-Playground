f65544aea6d27cc84d228ab9ea1b65cb88d2554f
Justin Weiler <jweiler@stanford.edu>
2023-12-23 13:15:26 -0800

    optimize counter

--- lib/counter.sv
+++ lib/counter.sv
@@ -3,13 +3,42 @@ module counter #(parameter C=1) (
 	output logic t
 );
 
-	localparam N = $clog2(C + 1);
-	logic [N-1:0] n;
-	logic [N-1:0] next_n;
+generate
+	if (C == 1) begin : counter_eq_one
+		assign t = 1'b1;
+	end else begin : counter_gt_one
 
-	assign next_n = n == C - 1 ? '0 : n + 1'd1;
-	assign t = n == C - 1;
+		localparam N = $clog2(C);
 
-	dffar #(N) flop (.d(next_n), .q(n), .clk(clk), .r(r));
+		logic [N-1:0] n;
+		logic [N-1:0] next_n;
+
+		logic n_is_max;
+		logic next_n_is_max;
+
+		assign t = n_is_max;
+
+		always_comb begin
+			next_n_is_max = n == C - 2;
+
+			next_n = n_is_max ? 0 : n + 1;
+		end
+
+		dffsr #(N) n_flop (
+			.d(next_n),
+			.q(n),
+			.r(r),
+			.clk(clk)
+		);
+
+		dffsr n_is_max_flop (
+			.d(next_n_is_max),
+			.q(n_is_max),
+			.r(r),
+			.clk(clk)
+		);
+
+	end
+endgenerate
 
 endmodule
